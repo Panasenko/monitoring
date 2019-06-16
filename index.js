73a177ec46c07e318c4c3c739339e572703cc2e8
Maks <panasenkomaks@gmail.com>
2019-06-17 01:16:40 +0300

    Последние правки

--- index.js
+++ index.js
@@ -10,15 +10,4 @@ async function main() {
     console.log(test1)
 
 
-
-/*    const Zabbix = new RequestZabbix('http://192.168.0.103/zabbix/api_jsonrpc.php')
-    let data = await Zabbix.login('Admin', 'zabbix')
-    await Zabbix.allHosts()
-
-    const Metods = new Methods('http://192.168.0.103/zabbix/api_jsonrpc.php')
-
-    console.log(await Metods.zabbixVersion())*/
-
 }
-
-main()
