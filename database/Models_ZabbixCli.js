fca55c5879c899112869329e47d8a635a251e341
Maks <panasenkomaks@gmail.com>
2019-07-23 21:30:33 +0300

    Исправлена ошибка с добавлением лишней коллекции

--- database/Models_ZabbixCli.js
+++ database/Models_ZabbixCli.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+
 const ItemsSchema = new mongoose.Schema({
     "name": {
         type: String,
@@ -17,8 +18,6 @@ const ItemsSchema = new mongoose.Schema({
         type: String
     }
 })
-
-//mongoose.model('ItemsSchema', ItemsSchema) //TODO: проверить влияние
 
 const AuthZabbixSchema = new mongoose.Schema({
     "name": {
